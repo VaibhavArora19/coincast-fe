3af404bc7fdc4e6792ea5c140b7e39f8c10c1699
Vaibhav Arora <aroravaibhav817@gmail.com>
2025-05-22 02:12:55 +0530

    changed url

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://zora-backend-uuaa.onrender.com";
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = "https://zora-backend-uuaa.onrender.com";
+// export const BACKEND_URL = "http://localhost:8000";
