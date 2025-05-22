5922aa66599212cb15be2aa4dd38805eeb1a77c7
Vaibhav Arora <82141868+VaibhavArora19@users.noreply.github.com>
2025-05-22 16:00:56 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://coincast-backend.onrender.com/";
+export const BACKEND_URL = "https://coincast-backend.onrender.com";
 // export const BACKEND_URL = "http://localhost:8000";
